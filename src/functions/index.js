37a2abfda360ab19680cee95337dff9623ef0f45
mcnallp5 <paul.mcnally5@mail.dcu.ie>
2021-01-20 01:11:40 +0000

    Added MongoDB connection

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,9 +1,19 @@
 const functions = require('firebase-functions');
+const mongoose = require('mongoose')
 const express = require('express');
+
+require('dotenv').config();
 
 const app = express();
 
 const { register, login } = require('./controllers/users');
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB connection established");
+})
 
 // Registration route
 app.post('/register', register);
