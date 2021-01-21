a5cd88d98b3df61cf5891aea10b1ceda06fed645
mcnallp5 <paul.mcnally5@mail.dcu.ie>
2021-01-21 20:25:57 +0000

    Removed Firestore

--- src/functions/controllers/users.js
+++ src/functions/controllers/users.js
@@ -23,23 +23,20 @@ exports.register = (req, res) => {
   if (!valid) return res.status(400).json(errors);
 
   // Register user
-  // TODO Replace firestore with MongoDB
   let token, userId;
-  db.doc(`/users/${newUser.company}`)
-    .get()
-    .then((doc) => {
-      // Check to see if company already registered
-      if (doc.exists) {
-        return res
-          .status(400)
-          .json({ company: 'This company is already registered' });
-      } else {
-        // Firebase authentication
-        return firebase
-          .auth()
-          .createUserWithEmailAndPassword(newUser.email, newUser.password);
-      }
-    })
+  const company = newUser.company;
+
+  // TODO Check if user account for company already exists in the DB
+//  NewUserDB.countDocuments({company: company}, function (err, count){
+//    if (count > 0){
+//      return res
+//        .status(400)
+//        .json({ company: 'This company is already registered' });
+//    }
+//  });
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
     .then((data) => {
       userId = data.user.uid;
       return data.user.getIdToken();
@@ -47,33 +44,21 @@ exports.register = (req, res) => {
     // Will use token whenever we need to access router that is protected
     .then((idToken) => {
       token = idToken;
-      const userCredentials = {
-        company: newUser.company,
-        email: newUser.email,
-        createdAt: new Date().toISOString(),
-        userId,
-      };
-      return db.doc(`/users/${newUser.company}`).set(userCredentials);
-    })
-    .then(() => {
-      const company = newUser.company;
+      const googleId = userId;
       const email = newUser.email;
-
+      // Add user to MongoDB
       const newUserEntry = new NewUserDB({
         company,
         email,
-        userId,
+        googleId,
       });
-
       newUserEntry.save();
-    })
-    .then(() => {
       return res.status(201).json({ token });
     })
     .catch((err) => {
       console.error(err);
       if (err.code == 'auth/email-already-in-use') {
-        return res.status(400).json({ email: 'Email is already in use' });
+         return res.status(400).json({ email: 'Email is already in use' });
       } else {
         return res.status(500).json({ error: err.code });
       }
