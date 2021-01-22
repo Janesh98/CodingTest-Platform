8ef41c79fc78f754ac84c714e87d1011f9780106
mcnallp5 <paul.mcnally5@mail.dcu.ie>
2021-01-22 18:15:14 +0000

    Refactor of bakend login

--- src/functions/controllers/users.js
+++ src/functions/controllers/users.js
@@ -40,29 +40,3 @@ exports.register = (req, res) => {
 //  });
 };
 
-exports.login = (req, res) => {
-  const user = {
-    email: req.body.email,
-    password: req.body.password,
-  };
-
-  const { valid, errors } = validateLoginData(user);
-
-  if (!valid) return res.status(400).json(errors);
-
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(user.email, user.password)
-    .then((data) => {
-      return data.user.getIdToken();
-    })
-    .then((token) => {
-      return res.json({ token });
-    })
-    .catch((err) => {
-      console.error(err);
-      if (err.code == 'auth/wrong-password') {
-        return res.status(403).json('Email or Password incorrect');
-      } else return res.status(500).json({ error: err.code });
-    });
-};
