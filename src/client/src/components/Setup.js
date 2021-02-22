578d112e78180b5eee10f6d67240a17d59ac8667
sharmaj3 <janesh.sharma3@mail.dcu.ie>
2021-02-22 17:23:33 +0000

    Update CI to detect build errors before merge

--- src/client/src/components/Setup.js
+++ src/client/src/components/Setup.js
@@ -9,12 +9,11 @@ import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import './css/Setup.css';
 import NavBar from './Navbar';
-import Card from "./Card"
+import Card from './Card';
 import { useHistory } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import { addChallenge } from '../endpoints';
-import Alert from '@material-ui/lab/Alert';
-
+// import Alert from '@material-ui/lab/Alert';
 
 const Setup = () => {
   const [title, setTitle] = useState('');
@@ -44,76 +43,75 @@ const Setup = () => {
   function refreshPage() {
     window.location.reload(false);
   }
-  
+
   const handleOnClickSave = async (e) => {
     e.preventDefault();
     if (title.length === 0 || probDesc.length === 0 || inFormat === 0) {
       setISError(true);
       return setError('All highlighted fields must not be empty');
     } else {
-    await addChallenge({
-      googleId: currentUser.uid,
-      testName: testName,
-      title: title,
-      problemDescription: probDesc,
-      inputFormat: inFormat,
-      returnFormat: returnFormat,
-      constraints: constraints,
-      sampleInput: sampleIn,
-      sampleOutput: sampleOut,
-      exampleExplanation: exampleExplanation,
-      testInput1: testIn1,
-      testOutput1: testOut1,
-      testInput2: testIn2,
-      testOutput2: testOut2,
-      testInput3: testIn3,
-      testOutput3: testOut3,
-      testInput4: testIn4,
-      testOutput4: testOut4,
-      testInput5: testIn5,
-      testOutput5: testOut5,
+      await addChallenge({
+        googleId: currentUser.uid,
+        testName: testName,
+        title: title,
+        problemDescription: probDesc,
+        inputFormat: inFormat,
+        returnFormat: returnFormat,
+        constraints: constraints,
+        sampleInput: sampleIn,
+        sampleOutput: sampleOut,
+        exampleExplanation: exampleExplanation,
+        testInput1: testIn1,
+        testOutput1: testOut1,
+        testInput2: testIn2,
+        testOutput2: testOut2,
+        testInput3: testIn3,
+        testOutput3: testOut3,
+        testInput4: testIn4,
+        testOutput4: testOut4,
+        testInput5: testIn5,
+        testOutput5: testOut5,
       });
       return refreshPage();
     }
-    };
+  };
 
   const handleOnClickContinue = async (e) => {
     try {
       e.preventDefault();
       history.push({
         pathname: '/questions',
-        state:{ newTestName : testName}});
+        state: { newTestName: testName },
+      });
     } catch {
       console.log('error');
     }
   };
 
   const handleOnClickExit = async (e) => {
-      try {
-        e.preventDefault();
-        history.push('/');
-      } catch {
-        console.log('error');
-      }
-    };
-  
+    try {
+      e.preventDefault();
+      history.push('/');
+    } catch {
+      console.log('error');
+    }
+  };
+
   return (
     <Container>
-      <NavBar/>
-    <div id="setup-container" style={{marginTop: 200}}>
-      <Grid container align="center" justify="center" direction="column">
-        <Container component="main" maxWidth="xs">
-          <div>
-            <Typography component="h3" variant="h5">
-              Setup A New Coding Test
-            </Typography>
-          </div>
-          <Card/>
-          <form>
-            <Typography component="h1">
-              Challenge Title 
-            </Typography>
-            <TextField
+      <NavBar />
+      <div id="setup-container" style={{ marginTop: 200 }}>
+        <Grid container align="center" justify="center" direction="column">
+          <Container component="main" maxWidth="xs">
+            <div>
+              <Typography component="h3" variant="h5">
+                Setup A New Coding Test
+              </Typography>
+            </div>
+            <Card />
+            <form>
+              <Typography component="h1">Challenge Title</Typography>
+              <TextField
                 variant="outlined"
                 margin="normal"
                 required
@@ -128,16 +126,14 @@ const Setup = () => {
                 helperText={error}
                 onChange={(input) => setTitle(input.target.value)}
               />
-              <Typography component="h1">
-                Problem Description
-              </Typography> 
+              <Typography component="h1">Problem Description</Typography>
               <TextField
                 variant="outlined"
                 margin="normal"
                 required
                 fullWidth
                 multiline
-                rows = {3}
+                rows={3}
                 id="problem description"
                 label="Problem Description"
                 placeholder="Problem Description of Challenge"
@@ -146,9 +142,7 @@ const Setup = () => {
                 helperText={error}
                 onChange={(input) => setProbDesc(input.target.value)}
               />
-              <Typography component="h1">
-                Input Format
-              </Typography> 
+              <Typography component="h1">Input Format</Typography>
               <TextField
                 variant="outlined"
                 margin="normal"
@@ -163,10 +157,8 @@ const Setup = () => {
                 helperText={error}
                 onChange={(input) => setInFormat(input.target.value)}
               />
-            
-              <Typography component="h1">
-                Return Format
-              </Typography> 
+
+              <Typography component="h1">Return Format</Typography>
               <TextField
                 variant="outlined"
                 margin="normal"
@@ -181,28 +173,23 @@ const Setup = () => {
                 helperText={error}
                 onChange={(input) => setReturnFormat(input.target.value)}
               />
-              <Typography component="h1">
-                Constraints
-              </Typography> 
+              <Typography component="h1">Constraints</Typography>
               <TextField
                 variant="outlined"
                 margin="normal"
                 required
                 fullWidth
                 multiline
-                rows = {2}
-                id= "constraints"
+                rows={2}
+                id="constraints"
                 label="Constraints"
                 placeholder="Constraints"
-                name="constraints"   
+                name="constraints"
                 error={isError}
-                helperText={error}    
+                helperText={error}
                 onChange={(input) => setConstraints(input.target.value)}
-                
               />
-              <Typography component="h1">
-                Sample Input 
-              </Typography> 
+              <Typography component="h1">Sample Input</Typography>
               <TextField
                 variant="outlined"
                 margin="normal"
@@ -214,12 +201,10 @@ const Setup = () => {
                 placeholder="Sample Input"
                 name="sample input"
                 error={isError}
-                helperText={error}             
+                helperText={error}
                 onChange={(input) => setSampleIn(input.target.value)}
               />
-              <Typography component="h1">
-                Sample Output 
-              </Typography> 
+              <Typography component="h1">Sample Output</Typography>
               <TextField
                 variant="outlined"
                 margin="normal"
@@ -229,162 +214,160 @@ const Setup = () => {
                 id="sample output"
                 label="Sample Output"
                 placeholder="Sample Output"
-                name="sample output"  
+                name="sample output"
                 error={isError}
-                helperText={error}          
+                helperText={error}
                 onChange={(input) => setSampleOut(input.target.value)}
               />
-              <Typography component="h1">
-                Example with Explanation  
-              </Typography> 
+              <Typography component="h1">Example with Explanation</Typography>
               <TextField
                 variant="outlined"
                 margin="normal"
                 required
                 fullWidth
                 multiline
-                rows = {2}
+                rows={2}
                 id="example with explanation"
                 label="Example with Explanation"
                 placeholder="Example with Explanation"
-                name="example with explanation" 
+                name="example with explanation"
                 error={isError}
-                helperText={error}            
+                helperText={error}
                 onChange={(input) => setExampleExplanation(input.target.value)}
               />
               <Typography component="h1">
-                Test Cases (Please provide at least 1 test case)  
+                Test Cases (Please provide at least 1 test case)
               </Typography>
-               <TextField
-                 variant="outlined"
-                 margin="normal"
-                 required
-                 id="test input 1"
-                 label="Input"
-                 placeholder="Input"
-                 name="Input"
-                 error={isError}
-                 helperText={error}
-                 onChange={(input) => setTestIn1(input.target.value)}
-               />
-               <TextField
-                 variant="outlined"
-                 margin="normal"
-                 required
-                 id="test output 1"
-                 label="Expected Output"
-                 placeholder="Expected Output"
-                 name="Output"       
-                 error={isError}
-                 helperText={error}      
-                 onChange={(input) => setTestOut1(input.target.value)}
-               />
-               <TextField
-                 variant="outlined"
-                 margin="normal"
-                 id="test input 2"
-                 label="Input"
-                 placeholder="Input"
-                 name="Input"              
-                 onChange={(input) => setTestIn2(input.target.value)}
-               />
-               <TextField
-                 variant="outlined"
-                 margin="normal"
-                 id="test output 2"
-                 label="Expected Output"
-                 placeholder=" Expected Output"
-                 name="Output"             
-                 onChange={(input) => setTestOut2(input.target.value)}
-               />
-               <TextField
-                 variant="outlined"
-                 margin="normal"
-                 id="test input 3"
-                 label="Input"
-                 placeholder="Input"
-                 name="Input"          
-                 onChange={(input) => setTestIn3(input.target.value)}
-               />
-               <TextField
-                 variant="outlined"
-                 margin="normal"
-                 id="test output 3"
-                 label="Expected Output"
-                 placeholder=" Expected Output"
-                 name="Output"
-                 onChange={(input) => setTestOut3(input.target.value)}
-               />
-               <TextField
-                 variant="outlined"
-                 margin="normal"
-                 id="test input 4"
-                 label="Input"
-                 placeholder="Input"
-                 name="Input"
-                 onChange={(input) => setTestIn4(input.target.value)}
-               />
-               <TextField
-                 variant="outlined"
-                 margin="normal"
-                 id="test output 4"
-                 label="Expected Output"
-                 placeholder=" Expected Output"
-                 name="Output"
-                 onChange={(input) => setTestOut4(input.target.value)}
-               />
-               <TextField
-                 variant="outlined"
-                 margin="normal"
-                 id="test input 5"
-                 label="Input"
-                 placeholder="Input"
-                 name="Input"
-                 onChange={(input) => setTestIn5(input.target.value)}
-               />
-               <TextField
-                 variant="outlined"
-                 margin="normal"
-                 id="test output 5"
-                 label="Expected Output"
-                 placeholder=" Expected Output"
-                 name="Output"
-                 onChange={(input) => setTestOut5(input.target.value)}
-               />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                id="test input 1"
+                label="Input"
+                placeholder="Input"
+                name="Input"
+                error={isError}
+                helperText={error}
+                onChange={(input) => setTestIn1(input.target.value)}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                id="test output 1"
+                label="Expected Output"
+                placeholder="Expected Output"
+                name="Output"
+                error={isError}
+                helperText={error}
+                onChange={(input) => setTestOut1(input.target.value)}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                id="test input 2"
+                label="Input"
+                placeholder="Input"
+                name="Input"
+                onChange={(input) => setTestIn2(input.target.value)}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                id="test output 2"
+                label="Expected Output"
+                placeholder=" Expected Output"
+                name="Output"
+                onChange={(input) => setTestOut2(input.target.value)}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                id="test input 3"
+                label="Input"
+                placeholder="Input"
+                name="Input"
+                onChange={(input) => setTestIn3(input.target.value)}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                id="test output 3"
+                label="Expected Output"
+                placeholder=" Expected Output"
+                name="Output"
+                onChange={(input) => setTestOut3(input.target.value)}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                id="test input 4"
+                label="Input"
+                placeholder="Input"
+                name="Input"
+                onChange={(input) => setTestIn4(input.target.value)}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                id="test output 4"
+                label="Expected Output"
+                placeholder=" Expected Output"
+                name="Output"
+                onChange={(input) => setTestOut4(input.target.value)}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                id="test input 5"
+                label="Input"
+                placeholder="Input"
+                name="Input"
+                onChange={(input) => setTestIn5(input.target.value)}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                id="test output 5"
+                label="Expected Output"
+                placeholder=" Expected Output"
+                name="Output"
+                onChange={(input) => setTestOut5(input.target.value)}
+              />
               <Button
-                id = "save-challenge"
+                id="save-challenge"
                 variant="contained"
                 color="primary"
                 size="large"
                 startIcon={<SaveIcon />}
                 onClick={(e) => handleOnClickSave(e)}
-                >
-                  Save Challenge 
-                </Button>
-                <Button
-                id = "continue"
+              >
+                Save Challenge
+              </Button>
+              <Button
+                id="continue"
                 variant="contained"
                 color="secondary"
                 size="large"
                 startIcon={<ArrowForwardIcon />}
                 onClick={(e) => handleOnClickContinue(e)}
-                >
-                  Continue 
-                </Button>
-                <Button
-                id = "exit"
+              >
+                Continue
+              </Button>
+              <Button
+                id="exit"
                 variant="contained"
                 color="primary"
                 size="large"
                 startIcon={<ExitToAppIcon />}
                 onClick={(e) => handleOnClickExit(e)}
-                >
-                  Exit
-                </Button>
-          </form>
-        </Container>
-      </Grid>
-    </div>
+              >
+                Exit
+              </Button>
+            </form>
+          </Container>
+        </Grid>
+      </div>
     </Container>
   );
 };
