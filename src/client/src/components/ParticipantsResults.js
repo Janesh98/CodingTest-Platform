417b50f8a5045fcfc6727c7202a493c7ffd74735
mcnallp5 <paul.mcnally5@mail.dcu.ie>
2021-04-01 20:20:21 +0100

    video playback of question responses

--- src/client/src/components/ParticipantsResults.js
+++ src/client/src/components/ParticipantsResults.js
@@ -16,18 +16,20 @@ import { useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import { getParticipantResults } from '../endpoints';
 import axios from 'axios';
+import PlayerCard from './PlayerCard'
 
 const ParticipantsResults = () => {
   const [tableData, setTableData] = useState([]);
   const [questionsTableData, setQuestionsTableData] = useState([]);
   const history = useHistory();
   const id = history.location.state.id;
+  const [testId, setTestId] = useState('');
   const email = history.location.state.email;
   // eslint-disable-next-line react-hooks/exhaustive-deps
   var questionsList = []
-
+ 
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  
+
 
   
   useEffect(() => {
@@ -35,20 +37,37 @@ const ParticipantsResults = () => {
       var res = await axios.post(getParticipantResults, {
         data: { _id: id },
       });
+      setTestId(res.data.data[0].TestId);
       await setTableData(res.data.data[0].codingTestResults[0].challenges.map((item) => ({ title: item.title, testCases : item.testResults.reduce(reducer)/item.testResults.length, challengeData: item})));
 
       var key;
       var i = 1;
       for (key in res.data.data[0].codingTestResults[0].questions[0]){
         var question = {};
+
         question = {
           question: res.data.data[0].codingTestResults[0].questions[0][key],
-          id: i
+          id: i,
         }
+
+
+     //   question = {
+     //     question: res.data.data[0].codingTestResults[0].questions[0][key],
+     //     id: i,
+     //     videoUrl: videoUrl
+     //   }
         i ++;
-        questionsList.push(question)
+
+        questionsList.push(question);
+
       }
-      await setQuestionsTableData(questionsList.map((item) => ({ id: item.id, question: item.question })));
+      await setQuestionsTableData(questionsList.map((item) => ({ id: item.id, question: item.question, videoUrl: item.videoUrl })));
+
+   //   storage.ref().child(`${res.data.data[0].TestId}/${id}/1.mp4`).getDownloadURL()
+   //   .then((url) => {
+   //     setVideoUrl(url);
+   //   })
+    
     };
     rows();
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -139,14 +158,7 @@ const ParticipantsResults = () => {
                         {row.question}
                       </TableCell>
                       <TableCell>
-                        <Button
-                          id="addParticipants"
-                          variant="contained"
-                          color="primary"
-                          size="small"
-                        >
-                          View Response
-                        </Button>
+                        <PlayerCard testId={testId} participantId={id} id={row.id} question={row.question}/>
                       </TableCell>
                     </TableRow>
                   ))}
