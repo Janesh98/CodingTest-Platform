b24bf217e5d86d6cc97ddb2e4aaad7de5da0d394
jsharma <janesh.sharma3@mail.dcu.ie>
2021-04-23 23:50:05 +0100

    Update RCE to Https & add coding test e2e

---
+++ src/functions/end-to-end testing/cypress/integration/my-tests/codingTest-actions.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress"/>
+
+beforeEach(() => {
+  cy.visit(
+    'http://localhost:3000/codingtest/603eb0c20b376e4320e3b3be/607c75fda139d62d1817d7bb'
+  );
+  cy.wait(4000);
+});
+
+describe('Coding Test', () => {
+  it('Complete Coding Test in Python', () => {
+    // Q1
+    cy.get('[data-testid="editor"]')
+      .get('textarea')
+      .type('print("hello world")');
+    cy.get('#runcode').get('button').contains('Run Code').click().wait(10000);
+    cy.get('#vertical-tab-1').click();
+    cy.get('#vertical-tab-1').get('.MuiTab-wrapper').get('svg').should('exist');
+    // Q2
+    cy.get('#scrollable-auto-tab-1').click();
+    cy.get('[data-testid="editor"]')
+      .get('textarea')
+      .type('print("hello world")');
+    cy.get('#runcode').get('button').contains('Run Code').click().wait(10000);
+    cy.get('#vertical-tab-1').click();
+    cy.get('#vertical-tab-1').get('.MuiTab-wrapper').get('svg').should('exist');
+    // Submit Test
+    cy.get('[data-testid="submit"]').contains('Submit Test').click();
+  });
+});
