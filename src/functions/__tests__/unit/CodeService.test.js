88169fbd4d91d83f9c6cb04b71afad351887be32
sharmaj3 <janesh.sharma3@mail.dcu.ie>
2021-04-26 11:20:31 +0100

    Add CodeService Unit Tests

---
+++ src/functions/__tests__/unit/CodeService.test.js
@@ -0,0 +1,52 @@
+const axios = require('axios');
+jest.mock('axios');
+const submitCode = require('../../services/CodeService');
+
+describe('CodeService', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  it('should execute code in Python and return output', async () => {
+    const mockData = {};
+    jest.spyOn(axios, 'request').mockResolvedValueOnce(mockData);
+    const code = 'print("hello world")';
+    const language = 'python';
+    const stdin = '1 2 3 4 5';
+    const result = await submitCode(code, language, stdin);
+    expect(result).toEqual(mockData);
+  });
+  it('should execute code in Java and return output', async () => {
+    const mockData = {};
+    jest.spyOn(axios, 'request').mockResolvedValueOnce(mockData);
+    const code = 'print("hello world")';
+    const language = 'java';
+    const stdin = '1 2 3 4 5';
+    const result = await submitCode(code, language, stdin);
+    expect(result).toEqual(mockData);
+  });
+  it('should execute code in JavaScript and return output', async () => {
+    const mockData = {};
+    jest.spyOn(axios, 'request').mockResolvedValueOnce(mockData);
+    const code = 'print("hello world")';
+    const language = 'javascript';
+    const stdin = '1 2 3 4 5';
+    const result = await submitCode(code, language, stdin);
+    expect(result).toEqual(mockData);
+  });
+  it('should throw Error as language does not exist', async () => {
+    const mockData = {};
+    jest.spyOn(axios, 'request').mockResolvedValueOnce(mockData);
+    const code = 'print("hello world")';
+    const language = 'doesNotExist';
+    const stdin = '1 2 3 4 5';
+
+    let message = false;
+    try {
+      await submitCode(code, language, stdin);
+    } catch (e) {
+      message = e.message;
+    }
+    console.log(message);
+    expect(message).toBe(`Language '${language}' is not supported`);
+  });
+});
---
+++ src/functions/services/CodeService.js
@@ -0,0 +1,39 @@
+const axios = require('axios').default;
+
+const submitCode = async (code, language, stdin = '') => {
+  // judge0 api id to refer to programming language
+  languageId = 71;
+  switch (language) {
+    case 'python':
+      languageId = 71;
+      break;
+    case 'java':
+      languageId = 62;
+      break;
+    case 'javascript':
+      languageId = 63;
+      break;
+    default:
+      throw new Error(`Language '${language}' is not supported`);
+  }
+
+  const options = {
+    method: 'POST',
+    url: 'https://judge0-ce.p.rapidapi.com/submissions',
+    params: { base64_encoded: 'true', wait: 'true' },
+    headers: {
+      'content-type': 'application/json',
+      'x-rapidapi-key': '671207b829msh7bf3336d5f94edbp13fecbjsn452157ddab57',
+      'x-rapidapi-host': 'judge0-ce.p.rapidapi.com',
+    },
+    data: {
+      language_id: languageId,
+      source_code: code,
+      stdin: stdin,
+    },
+  };
+
+  return await axios.request(options);
+};
+
+module.exports = submitCode;
