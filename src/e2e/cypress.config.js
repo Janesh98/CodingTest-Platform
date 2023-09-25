1e83c3f9c9a68c450f3d452b393018ac070dedc8
Janesh Sharma <52086988+Janesh98@users.noreply.github.com>
2023-09-25 20:46:52 +0100

    Update cypress.config.js

--- src/e2e/cypress.config.js
+++ src/e2e/cypress.config.js
@@ -7,6 +7,20 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      // chrome 117 bug fix, https://github.com/cypress-io/cypress-documentation/issues/5479
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+      if (browser.name === 'chrome' && browser.isHeadless) {
+        launchOptions.args = launchOptions.args.map((arg) => {
+          if (arg === '--headless') {
+            return '--headless=new'
+          }
+      
+          return arg
+        })
+      }
+      
+      return launchOptions
+      })
       return require('./cypress/plugins/index.js')(on, config);
     },
     baseUrl: 'https://coding-test-platform.web.app',
